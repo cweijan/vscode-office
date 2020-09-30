7fc8c3ab5276da659d4c4784f97c3aa51f9c8569
cweijan <cweijan@163.com>
2020-09-30 18:02:29 +0800

    adjust style

--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,9 @@ import { ExcelEditor } from './excelEditor';
 
 export function activate(context: vscode.ExtensionContext) {
 
-	vscode.window.registerCustomEditorProvider("cweijan.viewExcel", new ExcelEditor(context));
+	vscode.window.registerCustomEditorProvider("cweijan.viewExcel", new ExcelEditor(context),{webviewOptions:{
+		retainContextWhenHidden:true
+	}});
 
 }
 
