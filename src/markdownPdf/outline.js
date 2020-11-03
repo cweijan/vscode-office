7f1341d09a8ce6f95eff9671cfb4fb9d9d8920cf
cweijan <cweijan@163.com>
2020-11-03 16:56:32 +0800

    增加容错

--- src/markdownPdf/outline.js
+++ src/markdownPdf/outline.js
@@ -23,7 +23,9 @@ function inflateDict(array, $, dict) {
     for (let index = 0; index < array.length; index++) {
         const li = $(array[index]);
         const a = li.children('a');
+        if (!a) continue;
         const key = getKey(a);
+        if (!dict[key]) { continue; }
         dict[key].title = a.text();
         dict[key].isLast = array.length == 1 || index == array.length - 1;
         const childs = $(array[index]).children('ol').children('li');
