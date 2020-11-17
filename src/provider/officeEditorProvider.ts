704b63c98cb3bf7985ea9fad5bb1d0376404b74e
cweijan <cweijan@163.com>
2020-11-17 10:26:37 +0800

    implment status bar

--- src/provider/officeEditorProvider.ts
+++ src/provider/officeEditorProvider.ts
@@ -17,9 +17,13 @@ const streamPipeline = util.promisify(require('stream').pipeline);
 export class OfficeEditorProvider implements vscode.CustomTextEditorProvider {
 
     private extensionPath: string;
+    private countStatus: vscode.StatusBarItem;
+    private cursorStatus: vscode.StatusBarItem;
 
     constructor(private context: vscode.ExtensionContext) {
         this.extensionPath = context.extensionPath;
+        this.countStatus = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+        this.cursorStatus = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 120);
     }
 
     private getFolders(): vscode.Uri[] {
@@ -107,6 +111,15 @@ export class OfficeEditorProvider implements vscode.CustomTextEditorProvider {
                 content: readFileSync(uri.fsPath, 'utf8'),
                 folderPath: webview.asWebviewUri(folderPath).toString()
             })
+        }).on("cursorActivity", (cursor) => {
+            this.cursorStatus.text = `Ln ${cursor.line}, Col ${cursor.ch}`
+            this.cursorStatus.show()
+        }).on("focus", ({ count, lineCount }) => {
+            this.countStatus.text = `Line ${lineCount}    Count ${count}`
+            this.countStatus.show()
+        }).on("blur", () => {
+            this.countStatus.hide()
+            this.cursorStatus.hide()
         }).on("save", (content) => {
             this.updateTextDocument(document, content)
         }).on("codemirrorEdit", (content) => {
