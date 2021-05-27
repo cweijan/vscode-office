4858426143ef52cb9be297ac1d2296ee9c78d8b7
cweijan <cweijan@163.com>
2021-05-27 22:18:16 +0800

    Remove markdown-hyper.

--- src/provider/officeEditorProvider.ts
+++ src/provider/officeEditorProvider.ts
@@ -97,18 +97,7 @@ export class OfficeEditorProvider implements vscode.CustomTextEditorProvider {
         const webview = handler.panel.webview;
 
         const content = readFileSync(uri.fsPath, 'utf8');
-        const type = vscode.workspace.getConfiguration("vscode-office").get<string>("markdownType");
-        let path = "markdown-hyper";
-        if (type == "vditor") {
-            path = "vditor";
-        }
-        if (type == "auto") {
-            path = "vditor";
-            const length=content?.match(/\$/g)?.length;
-            if(length && length>30){
-                path = "markdown-hyper";
-            }
-        }
+        let path = "vditor";
 
         Holder.activeUrl = uri;
         handler.panel.onDidChangeViewState(e => {
