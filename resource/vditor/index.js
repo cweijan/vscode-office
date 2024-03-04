4b6a2ec35a56b4f4f069bc78b57266917b98fa75
cweijan <cweijan@163.com>
2024-03-05 01:10:08 +0800

    Extract color as variable.

--- resource/vditor/index.js
+++ resource/vditor/index.js
@@ -92,9 +92,14 @@ handler.on("open", (md) => {
 
 
 function addAutoTheme(rootPath) {
+  importCSS(rootPath, 'base.css')
+  importCSS(rootPath, 'theme/auto.css')
+}
+
+function importCSS(rootPath, path) {
   const style = document.createElement('link');
   style.rel = "stylesheet";
   style.type = "text/css";
-  style.href = `${rootPath}/css/theme.css`;
+  style.href = `${rootPath}/css/${path}`;
   document.documentElement.appendChild(style)
 }
