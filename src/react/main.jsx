c572fdac798fa5f76be6aa257affd85d4d007d54
cweijan <cweijan@163.com>
2024-03-27 14:07:30 +0800

    Init react route

--- src/react/main.jsx
+++ src/react/main.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ExcelViewer from './view/ExcelViewer.jsx'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div style={{ 'text-align': 'center' }}>
+        <h1>Hello World</h1>
+        <Link to="about">About Us</Link>
+      </div>
+    ),
+  },
+  { path: "about", element: <div>About</div>, },
+  { path: "excel", element: <ExcelViewer />, },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
