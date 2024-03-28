bcbe65cf10bbc609c2acfd1c2ccd17c2b52dfe25
cweijan <cweijan@163.com>
2024-03-29 01:39:53 +0800

    Support scroll image via mouse wheel.

--- src/react/view/image/Image.tsx
+++ src/react/view/image/Image.tsx
@@ -1,25 +1,31 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import ImageGallery from "react-image-gallery";
 import "react-image-gallery/styles/css/image-gallery.css";
 import { handler } from '../../util/vscode';
 import './Image.less';
 
 export default function Image() {
+    const gallery = useRef(null)
     const [info, setInfo] = useState({ images: [], current: 0 } as any);
 
     useEffect(() => {
         handler.on('images', info => {
             setInfo(info)
         }).emit('images')
+        let previous = 0;
+        addEventListener('wheel', (e) => {
+            if (new Date().getTime() - previous < 300) return;
+            const current = gallery.current.getCurrentIndex();
+            gallery.current.slideToIndex(current + (e.deltaY > 0 ? 1 : -1))
+            previous = new Date().getTime();
+        })
     }, [])
-
     const images = info.images.map((image) => ({
         original: image.src,
         thumbnail: image.src
     }))
-    //https://github.com/xiaolin/react-image-gallery
     return (
-        <ImageGallery items={images} startIndex={info.current} lazyLoad={true}
+        <ImageGallery ref={gallery} items={images} startIndex={info.current} lazyLoad={true}
             slideDuration={0} showIndex={true} showFullscreenButton={false} showPlayButton={false}
         />
     );
