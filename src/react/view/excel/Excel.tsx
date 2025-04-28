c5dccbe528752be5e8d3d6273a9c4dc899dfecd4
cweijan <weijancc@gmail.com>
2025-04-28 14:22:40 +0800

    Add VSCode logo display for CSV files in Excel Viewer

--- src/react/view/excel/Excel.tsx
+++ src/react/view/excel/Excel.tsx
@@ -5,9 +5,11 @@ import './Excel.less';
 import { loadSheets } from "./excel_reader.ts";
 import Spreadsheet from './x-spreadsheet/index';
 import { export_xlsx } from "./excel_writer.ts";
+import VSCodeLogo from "../vscode.tsx";
 
 export default function Excel() {
     const loading = useRef(true)
+    const fileType = useRef<string>(null)
     const s2Ref = useRef<Spreadsheet>(null)
     const [sheets, setSheets] = useState<any[]>([])
     useEffect(() => {
@@ -18,9 +20,11 @@ export default function Excel() {
             console.log('Loading Excel file...');
             fetch(path).then(response => response.arrayBuffer()).then(res => {
                 const { sheets, maxLength, maxCols } = loadSheets(res, ext);
+                fileType.current = ext;
                 container.innerHTML = ''
                 const spreadSheet = new Spreadsheet(container, {
                     showBottomBar: false,
+                    showToolbar: !ext?.match(/csv/i),
                     row: {
                         len: maxLength + 50,
                         height: 30,
@@ -37,17 +41,6 @@ export default function Excel() {
                         export_xlsx(spreadSheet, ext);
                     }
                 });
-                const child = document.querySelector('.x-spreadsheet-toolbar-btns>div:nth-child(3)>.x-spreadsheet-icon') as HTMLElement;
-                if (ext?.match(/csv/i)) {
-                    child.innerHTML = '<svg height="18" viewBox="-11.9 -2 1003.9 995.6" width="18" xmlns="http://www.w3.org/2000/svg"><path d="m12.1 353.9s-24-17.3 4.8-40.4l67.1-60s19.2-20.2 39.5-2.6l619.2 468.8v224.8s-.3 35.3-45.6 31.4z" fill="#2489ca"/><path d="m171.7 498.8-159.6 145.1s-16.4 12.2 0 34l74.1 67.4s17.6 18.9 43.6-2.6l169.2-128.3z" fill="#1070b3"/><path d="m451.9 500 292.7-223.5-1.9-223.6s-12.5-48.8-54.2-23.4l-389.5 354.5z" fill="#0877b9"/><path d="m697.1 976.2c17 17.4 37.6 11.7 37.6 11.7l228.1-112.4c29.2-19.9 25.1-44.6 25.1-44.6v-671.2c0-29.5-30.2-39.7-30.2-39.7l-197.7-95.3c-43.2-26.7-71.5 4.8-71.5 4.8s36.4-26.2 54.2 23.4v887.5c0 6.1-1.3 12.1-3.9 17.5-5.2 10.5-16.5 20.3-43.6 16.2z" fill="#3c99d4"/></svg>'
-                    child.parentElement.dataset.tooltip='Edit In VS Code'
-                    child.onclick = e => {
-                        handler.emit("editInVSCode", true)
-                        e.stopPropagation();
-                    }
-                } else {
-                    child.parentElement.style.display = 'none'
-                }
                 s2Ref.current = spreadSheet;
                 loading.current = false;
                 spreadSheet.loadData(sheets);
@@ -84,6 +77,9 @@ export default function Excel() {
             <Tabs items={items} onChange={changeTab} tabBarGutter={20}
                 style={{ marginLeft: '50px' }}
             />
+            {
+                fileType.current?.match(/csv/i) ? <VSCodeLogo /> : null
+            }
         </div>
     )
 }
